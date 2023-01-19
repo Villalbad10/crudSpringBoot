1d5f61ed396ad12491cfddb192f02d5cc6f9d75a
Diego Alexander Villalba Cely <dvillalba@cohan.org.co>
2023-01-19 15:06:39 -0500

    solucion error back

--- src/main/java/com/crud/controller/EmpleadoController.java
+++ src/main/java/com/crud/controller/EmpleadoController.java
@@ -3,18 +3,18 @@ package com.crud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.entities.EmpleadoEntities;
 import com.crud.service.EmpleadoService;
 
-@Controller
+@RestController
 @RequestMapping("/empleados")
 public class EmpleadoController {
 
